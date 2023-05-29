0a6457ecabf28ea4b634c59a3ef4081d530248fd
Abhinay2210 <abhinaygiri2210.com>
2023-05-30 00:55:44 +0530

    tokenring updated

--- PS7/TokenRing.java
+++ PS7/TokenRing.java
@@ -1,49 +1,64 @@
-import java.io.*;
-import java.lang.*;
+import java.util.*;
 
-class TokenRing {
+public class TokenRing{
 
-	public static void main(String args[]) throws Throwable {
+	public static void main(String[] args){
 	
-	DataInputStream dis = new DataInputStream(System.in);
-	System.out.println("Enter the num of nodes:");
-
-	int n = Integer.parseInt(dis.readLine());
-	// Decides the number of nodes forming the ring
-	int token = 0;
-	int ch = 1;
-	for (int i = 0; i<n; i++)
-
-	System.out.print(" " + i);
-	System.out.println(" "+ 0);
-
-	try {
-		while (ch == 1) {
 		
-			System.out.println("Enter sender :");
-			int s = Integer.parseInt(dis.readLine());
-			
-			System.out.println("Enter receiver :");
-			int r = Integer.parseInt(dis.readLine());
-			
-			System.out.println("Enter Data:");
-			String d = dis.readLine();
-			
-			System.out.println("Token passing:");
-			for (int i=token; i != s; i++)
-				System.out.print(" "+ i + "->");
-			
-			System.out.println(" " + s);
-
-			System.out.println("Sender " + s + " sending data: " + d);
-			
-			for (int i=s+1; i != r; i=(i+1)%n)
-				System.out.println("data " + d.hashCode() + " forwarded by " + i);
-			System.out.println("Receiver " + r + " received data: " + d);
-	 		token = s;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter number of nodes you wanted in ring: ");
+		int n = sc.nextInt();
+		
+		System.out.println("Formed Token Ring: ");
+		
+		for(int i=0;i<n;i++){
+		
+			System.out.print(i + " ");
 		}
-
-	} catch (Exception e) {}
-
-}	
+		
+		System.out.println("0");
+		
+		int choice = 0;
+		
+		do{
+		
+			System.out.print("Enter Sender: ");
+			int sender = sc.nextInt(); 
+			
+			System.out.print("Enter Receiver: ");
+			int receiver = sc.nextInt();
+			
+			System.out.print("Enter data: ");
+			int data = sc.nextInt();
+			
+			int token = 0;
+			
+			System.out.print("Token Passing: ");
+			for(int i=token;i<sender;i++){
+			
+			System.out.print(" "+ i +"->");
+			
+			}
+			
+			System.out.println(" "+ sender);
+			
+			System.out.println("Sender: "+ sender + " Data: " + data);
+			
+			for(int i=sender; i!=receiver; i=(i+1)%n){
+			
+			System.out.println("Data: "+ data + " is forwarded by " + i);
+			
+			}
+			System.out.println("Data: "+ data + " is received by " + receiver);
+			
+			token = sender;
+			
+			System.out.println("Continue: 1, exit: 0");
+			choice = sc.nextInt();
+			
+		}while(choice == 1);
+		
+		
+	}
 }
